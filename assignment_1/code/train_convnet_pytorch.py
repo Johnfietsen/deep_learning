206e5e2affed027260ab71b7035fe793fd180bc9
Johnfietsen <lucstefelmanns@live.nl>
2019-04-21 15:27:04 +0000

    import torch added

--- assignment_1/code/train_convnet_pytorch.py
+++ assignment_1/code/train_convnet_pytorch.py
@@ -6,6 +6,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import torch
+import torch.nn as nn
 import argparse
 import numpy as np
 import os
