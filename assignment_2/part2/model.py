412e6d30ce636596573e6033f301ad564efbc1ab
Johnfietsen <lucstefelmanns@live.nl>
2019-05-02 20:04:00 +0000

    output to device

--- assignment_2/part2/model.py
+++ assignment_2/part2/model.py
@@ -44,11 +44,11 @@ class TextGenerationModel(nn.Module):
 
         out, (h, c) = self._lstm(x)
 
-        return self._linear(out).to(self._device), h, c
+        return self._linear(out).to(self._device), h.to(device), c.to(device)
 
 
     def generate_character(self, x, h, c):
 
         out, (h, c) = self._lstm(x, (h, c))
 
-        return self._linear(out).to(self._device), h, c
+        return self._linear(out).to(self._device), h.to(device), c.to(device)
